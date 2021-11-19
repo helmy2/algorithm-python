d0296457a0f6b7b89bf0ee4f103415e50c55de94
Helmy <mohamed0132525715@gmail.com>
2021-11-19 15:31:07 +0200

    Optimized Bubble Sort

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -8,9 +8,13 @@ from swap import *
 
 def bubble_sort(arr):
     for i in range(len(arr)):
+        swapped = False
         for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 swap(arr, j, j + 1)
+                swapped = True
+        if swapped:
+            break
 
 
 if __name__ == '__main__':
